773fb7e5d0c41806bc9def3cc47bf1d7247772c4
Kevo <me@kevo.link>
2022-07-16 12:47:54 +0200

    Delete scripts directory

--- scripts/compile_tailwind.sh
+++
@@ -1,7 +0,0 @@
-cd "$(dirname "$0")/.."
-cd tailwind
-
-npx tailwindcss -i ../ipmail/web/static/css/tailwind_base.css -o ../ipmail/web/static/css/production.css
-npx cleancss ../ipmail/web/static/css/production.css -o ../ipmail/web/static/css/production.min.css
-
-rm ../ipmail/web/static/css/production.css
